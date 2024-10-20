5476a73874e4aafd8ec0c8a3cae051cff424a4a2
Kier <kierchristian64@gmail.com>
2024-10-21 04:46:36 +0800

    add

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,21 +1,21 @@
-function AboutSection () {
-  return (
-      <>
-      <div id="about" class="full-screen-height px-4 pt-5 my-5 text-center border-bottom">
+    function AboutSection () {
+    return (
+        <>
+        <div id="about" class="full-screen-height px-4 pt-5 my-5 text-center border-bottom">
 
-      <h1 class="container text-center display-6 fw-bold text-body-emphasis lh-1 mb-3">About Me</h1>
-          <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">I'm still only a novice when it comes to anything but one thing to me is that I am persistent at things that I want. What I wanted is to learn anything about computers have a role to the industry and also to connect with other people. </p>
-         
-          </div>
-          <div class="overflow-hidden">
-          <div class="container px-5">
-              <img src="https://media1.tenor.com/m/7pyDepP8SOQAAAAC/cat-wif.gif" class="border-outline  img-fluid rounded-3 " alt="Example image" width="auto" height="200" loading="lazy"/>
-          </div>
-          </div>
-      </div>
-      </>
-  );
+        <h1 class="container text-center display-6 fw-bold text-body-emphasis lh-1 mb-3">About Me</h1>
+            <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">I'm still only a novice when it comes to anything but one thing to me is that I am persistent at things that I want. What I wanted is to learn anything about computers have a role to the industry and also to connect with other people. </p>
+           
+            </div>
+            <div class="overflow-hidden">
+            <div class="container px-5">
+                <img src="https://media1.tenor.com/m/7pyDepP8SOQAAAAC/cat-wif.gif" class="border-outline  img-fluid rounded-3 " alt="Example image" width="auto" height="200" loading="lazy"/>
+            </div>
+            </div>
+        </div>
+        </>
+    );
 }
 
 export default AboutSection;
